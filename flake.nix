10bbfbb73ed6b7a007adf1a2582ceece4fd12aae
Johannes Loetzsch <git@johannesloetzsch.de>
2020-10-21 19:17:43 +0200

    github:NixOS/nixos-hardware

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     home-manager.url = "github:nix-community/home-manager";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, home-manager, nixos-hardware }:
   let
     base-modules = [
       ./modules/defaults/nix.nix
@@ -24,6 +25,7 @@
       t470s = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = base-modules ++ [
+          nixos-hardware.nixosModules.lenovo-thinkpad-t470s
           ./modules/systems/t470s.nix
           ./modules/defaults/networking.nix
           ./modules/defaults/desktop.nix
